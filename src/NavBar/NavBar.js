1eb9ac65882bcd8705102f303f9f49e75037b544
Andreea Hanson <andreea.hanson7@gmail.com>
2020-05-19 22:45:48 -0600

    Add svg icons to nav links

--- src/NavBar/NavBar.js
+++ src/NavBar/NavBar.js
@@ -1,16 +1,50 @@
 import React from "react";
 import "./NavBar.scss";
 import { NavLink } from "react-router-dom";
+import statsIcon from "../assets/money-graph-with-up-arrow.svg";
+import recipesIcon from "../assets/chef.svg";
+import inventoryIcon from "../assets/check-list.svg";
+import groceryIcon from "../assets/grocery.svg";
+import userIcon from "../assets/user.svg";
 
 const NavBar = () => {
   return (
     <>
       <nav>
-        <NavLink to={"/profile"} className="route" >Profile</NavLink>
-        <NavLink to={"/grocery_list"} className="route" >Grocery List</NavLink>
-        <NavLink to={"/inventory"} className="route" >Inventory</NavLink>
-        <NavLink to={"/recipes"} className="route" >Recipes</NavLink>
-        <NavLink to={"/stats"} className="route" >Stats</NavLink>
+      <div className="vertical padded">
+          <img alt="statistics" src={userIcon} />
+        <NavLink to={"/profile"} className="route">
+          Profile
+        </NavLink>
+        </div>
+
+        <div className="vertical padded">
+          <img alt="statistics" src={groceryIcon} />
+        <NavLink to={"/grocery_list"} className="route">
+          Grocery List
+        </NavLink>
+        </div>
+
+        <div className="vertical padded">
+          <img alt="statistics" src={inventoryIcon} />
+          <NavLink to={"/inventory"} className="route">
+            Inventory
+          </NavLink>
+        </div>
+
+        <div className="vertical padded">
+          <img alt="statistics" src={recipesIcon} />
+          <NavLink to={"/recipes"} className="route">
+            Recipes
+          </NavLink>
+        </div>
+
+        <div className="vertical padded">
+          <img alt="statistics" src={statsIcon} />
+          <NavLink to={"/stats"} className="route">
+            Stats
+          </NavLink>
+        </div>
       </nav>
     </>
   );
